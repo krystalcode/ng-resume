b685f06050e3adf3b10d9977efd740a49b94dbf4
Dimitris Bozelos <dbozelos@gmail.com>
2024-02-11 16:10:31 +0000

    Added contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { MaterialModule } from '@angular/material';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 import { AwardsComponent } from './awards/awards.component';
 import { CodeComponent } from './code/code.component';
 
 const routes: Routes = [
   { path: 'code'               , component: CodeComponent         },
   { path: 'awards'             , component: AwardsComponent       },
+  { path: 'contact'            , component: ContactComponent      },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    ContactComponent,
     AwardsComponent,
     CodeComponent,
   ],
